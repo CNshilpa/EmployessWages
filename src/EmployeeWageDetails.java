09ccc74e512b9e8fb34c60a5ddbfb00ed5b044a0
CNshilpa <shilpa.cnv@gmail.com>
2022-06-18 22:30:01 +0530

    Addition : Part Time Wage

--- src/EmployeeWageDetails.java
+++ src/EmployeeWageDetails.java
@@ -5,18 +5,24 @@ public class EmployeeWageDetails {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-        int attendance=random.nextInt(2);
+        int attendance=random.nextInt(3);
         int wagePerHour=20;
         int fullDayHour=8;
         int wagePerDay=0;
-        if(attendance==1)
+        int partTimeHours=4;
+        if(attendance==0)
         {
-        	System.out.println("Employee Present");
-            wagePerDay=wagePerHour*fullDayHour;
-            System.out.println("Employee's daily wage is:" + wagePerDay);
-        }else {
         	System.out.println("Employee Absent");
         	System.out.println(wagePerDay);
+        }
+        else if(attendance==1)
+        {
+        	System.out.println("Employee Present for part time hours");
+            wagePerDay=wagePerHour*partTimeHours;
+            System.out.println("Employee's daily wage is:" + wagePerDay);
+        }else {
+        	wagePerDay=wagePerHour*fullDayHour;
+            System.out.println("Employee's daily wage is:" + wagePerDay);
         }
 	}
 }
