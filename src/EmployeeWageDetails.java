563d043b785b60416a8a4915f3f8e2e423d7b74a
CNshilpa <shilpa.cnv@gmail.com>
2022-06-18 23:34:33 +0530

    Addition : Attendance  Check

--- src/EmployeeWageDetails.java
+++ src/EmployeeWageDetails.java
@@ -5,38 +5,11 @@ public class EmployeeWageDetails {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-		int attendance = random.nextInt(3);
-		int wagePerHour = 20;
-		int fullDayHour = 8;
-		int wagePerDay = 0;
-		int partTimeHours = 4;
-		int monthlyHours = 0;
-		int monthlySalary = 0;
-		int day = 0;
-		while (monthlyHours != 100 && day != 20) {
-			day++;
+		int attendance = random.nextInt(2);
+		if (attendance == 1)
+			System.out.println("Employee Present");
+		else
+			System.out.println("Employee Absent");
 
-			switch (attendance) {
-			case 0:
-				System.out.println("Employee is absent");
-				break;
-			case 1:
-				System.out.println("Employee  is present");
-				wagePerDay = fullDayHour * wagePerHour;
-				monthlyHours = monthlyHours + fullDayHour;
-				System.out.println("Employee's daily wage is:" + wagePerDay);
-				break;
-			case 2:
-				System.out.println("Employee is present for part time");
-				wagePerDay = partTimeHours * wagePerHour;
-				monthlyHours = monthlyHours + partTimeHours;
-				System.out.println("Employee's daily wage is:" + wagePerDay);
-				break;
-
-			}
-			monthlySalary = monthlySalary + wagePerDay;
-			System.out.println(
-					" Day is: " + day + " monthlyHours: " + monthlyHours + " Monthly Salary is: " + monthlySalary);
-		}
 	}
 }
