1ec5a93f49ad865c96e0f6a975aa866c723dd1c0
CNshilpa <shilpa.cnv@gmail.com>
2022-06-18 21:53:48 +0530

    Addition : Attendance Check

--- src/EmployeeWageDetails.java
+++ src/EmployeeWageDetails.java
@@ -5,39 +5,11 @@ public class EmployeeWageDetails {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-
-		int wagePerHour = 20;
-		int fullDayHours = 8;
-		int partTimeHours = 4;
-		int wagePerDay = 0;
-		int monthlySalary = 0;
-		int monthlyHours = 0;
-		int days = 0;
-
-		while (monthlyHours != 100 && days != 20) {
-
-			int attendance = random.nextInt(3);
-			wagePerDay = 0;
-
-			switch (attendance) {
-			case 0:
-				System.out.println("Employee Absent.");
-				break;
-			case 1:
-				wagePerDay = partTimeHours * wagePerHour;
-				monthlyHours = monthlyHours + partTimeHours;
-				break;
-			default:
-				wagePerDay = fullDayHours * wagePerHour;
-				monthlyHours = monthlyHours + fullDayHours;
-
-			}
-			monthlySalary = monthlySalary + wagePerDay;
-
-			System.out
-					.println("Day: " + days + " Monthly Hours: " + monthlyHours + " Monthly Salary: " + monthlySalary);
-			days++;
-		}
-
+        int attendance=random.nextInt(2);
+        if(attendance==1)
+        	System.out.println("Employee Present");
+        else
+        	System.out.println("Employee Absent");
+		
 	}
 }
