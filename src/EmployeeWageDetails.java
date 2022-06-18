239efebefa542b7a972062af8084fe1e8b4b45bd
CNshilpa <shilpa.cnv@gmail.com>
2022-06-19 00:27:39 +0530

    Addition : Calculating Wages Per Month

--- src/EmployeeWageDetails.java
+++ src/EmployeeWageDetails.java
@@ -10,16 +10,29 @@ public class EmployeeWageDetails {
 		int fullDayHour = 8;
 		int wagePerDay = 0;
 		int partTimeHours = 4;
-		if (attendance == 0) {
-			System.out.println("Employee Absent");
-			System.out.println(wagePerDay);
-		} else if (attendance == 1) {
-			System.out.println("Employee Present for part time hours");
-			wagePerDay = wagePerHour * partTimeHours;
-			System.out.println("Employee's daily wage is:" + wagePerDay);
-		} else {
-			wagePerDay = wagePerHour * fullDayHour;
-			System.out.println("Employee's daily wage is:" + wagePerDay);
+		int monthlyHours = 0;
+		int monthlySalary = 0;
+		for (int day = 1; day <= 20; day++) {
+
+			switch (attendance) {
+			case 0:
+				System.out.println("Employee is absent");
+				break;
+			case 1:
+				System.out.println("Employee is present");
+				wagePerDay = fullDayHour * wagePerHour;
+				monthlyHours = monthlyHours + fullDayHour;
+				System.out.println("Employee's daily wage is:" + wagePerDay);
+				break;
+			case 2:
+				System.out.println("Employee is present for part time");
+				wagePerDay = partTimeHours * wagePerHour;
+				System.out.println("Employee's daily wage is:" + wagePerDay);
+				break;
+
+			}
+			monthlySalary = monthlySalary + wagePerDay;
+			System.out.println(" Day is: " + day + " Monthly Salary is: " + monthlySalary);
 		}
 	}
 }
