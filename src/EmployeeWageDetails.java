8c7e530b9de83844afa6c905cacc19dd4d129745
CNshilpa <shilpa.cnv@gmail.com>
2022-06-18 22:42:12 +0530

    addition : Employee Wage Using SwitchCase

--- src/EmployeeWageDetails.java
+++ src/EmployeeWageDetails.java
@@ -5,24 +5,25 @@ public class EmployeeWageDetails {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-        int attendance=random.nextInt(3);
-        int wagePerHour=20;
-        int fullDayHour=8;
-        int wagePerDay=0;
-        int partTimeHours=4;
-        if(attendance==0)
-        {
-        	System.out.println("Employee Absent");
-        	System.out.println(wagePerDay);
-        }
-        else if(attendance==1)
-        {
-        	System.out.println("Employee Present for part time hours");
-            wagePerDay=wagePerHour*partTimeHours;
-            System.out.println("Employee's daily wage is:" + wagePerDay);
-        }else {
-        	wagePerDay=wagePerHour*fullDayHour;
-            System.out.println("Employee's daily wage is:" + wagePerDay);
-        }
+		int attendance = random.nextInt(3);
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int wagePerDay = 0;
+		int partTimeHours = 4;
+		switch (attendance) {
+		case 0:
+			System.out.println("Employee Absent");
+			System.out.println(wagePerDay);
+			break;
+
+		case 1:
+			System.out.println("Employee Present for part time hours");
+			wagePerDay = wagePerHour * partTimeHours;
+			System.out.println("Employee's daily wage is:" + wagePerDay);
+			break;
+		default:
+			wagePerDay = wagePerHour * fullDayHour;
+			System.out.println("Employee's daily wage is:" + wagePerDay);
+		}
 	}
 }
